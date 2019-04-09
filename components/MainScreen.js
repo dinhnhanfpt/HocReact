cfd08c0d41529d087c9a36d2975ae998dd670f6d
DinhNhan <dinhnhanfpt@gmail.com>
2019-04-09 17:53:14 +0700

    component 09/04/19

---
+++ components/MainScreen.js
@@ -0,0 +1,9 @@
+import React,{ Component } from 'react';
+import { Text } from 'react-native';
+export default class MainScreen extends Component{
+    render(){
+        return(
+            <Text>This is main screen</Text>
+        );
+    }
+}
--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import MainScreen from './components/MainScreen';
+import Robot from './components/Robot';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Robot);
