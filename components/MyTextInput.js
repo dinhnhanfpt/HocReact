a911c14d3a2bafd5e02c2b8bb40510e47b05554c
DinhNhan <dinhnhanfpt@gmail.com>
2019-05-04 09:01:43 +0700

    multiLine text

--- components/MyTextInput.js
+++ components/MyTextInput.js
@@ -1,48 +1,68 @@
-import React, {Component} from 'react'
-import {View, TextInput,Text} from 'react-native'
-export default class MyTextInput extends Component{
-    constructor(props){
+import React, { Component } from 'react'
+import { View, TextInput, Text } from 'react-native'
+export default class MyTextInput extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            email : "",
-            passoword:""
+        this.state = {
+            email: "",
+            passoword: "",
+            myMultiline:""
         };
     }
-    render(){
-        return(
-            <View style={{ padding:10, flex:1 }}>
-                <TextInput style={{height:40, borderColor:'gray', borderWidth:1, margin:10}}
-                keyboardType='email-address'
-                placeholder='please enter your email'
-                onChangeText ={
-                    (text)=>{
-                        this.setState(
-                            (priviousState)=>{
-                                return{textchange : text};
-                            }
-                        );
+    render() {
+        return (
+            <View style={{ padding: 10, flex: 1 }}>
+                <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 10 }}
+                    keyboardType='email-address'
+                    placeholder='please enter your email'
+                    onChangeText={
+                        (text) => {
+                            this.setState(
+                                (priviousState) => {
+                                    return { textchange: text };
+                                }
+                            );
+                        }
                     }
-                }
                 />
 
                 <Text>{this.state.textchange}</Text>
 
-                <TextInput style={{height:40, borderColor:'gray', borderWidth:1, margin:10}}
-                keyboardType='default'
-                placeholder='please enter your passoword'
-                secureTextEntry={true}
-                onChangeText = {
-                    (text)=>{
-                        this.setState(
-                            (priviousState)=>{
-                                return{passoword:text}
-                            }
-                        );
+                <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 10,  }}
+                    keyboardType='default'
+                    placeholder='please enter your passoword'
+                    secureTextEntry={true}
+                    onChangeText={
+                        (text) => {
+                            this.setState(
+                                (priviousState) => {
+                                    return { passoword: text }
+                                }
+                            );
+                        }
+                    } />
+                <Text>{this.state.passoword}</Text>
+                <TextInput style={{
+                    height: 100,
+                    borderWidth: 1,
+                    borderColor: 'gray',
+                    margin:10
+                }}
+                    multiline={true}
+                    onChangeText={
+                        (text) => {
+                            this.setState((priviousTate) => {
+                                return {
+                                    myMultiline: text
+                                }
+                            });
+                        }
                     }
-                } />
-                 <Text>{this.state.passoword}</Text>
+                    
+                />
+                <Text>{this.state.myMultiline}</Text>
             </View>
         );
     }
-    
+
 }
